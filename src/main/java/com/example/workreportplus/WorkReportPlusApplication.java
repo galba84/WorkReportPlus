b2537ecafd8a82b3f23f57ccf06b2cb5bfa0d225
seredalex <sered2a@gmail.com>
2025-02-16 13:36:25 +0200

    Initial commit

---
+++ src/main/java/com/example/workreportplus/WorkReportPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.workreportplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkReportPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkReportPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workreportplus/WorkReportPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workreportplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkReportPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
